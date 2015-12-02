a23a4a5ea7d170d983914022dd6d385c6c808cc8
gpk659 <pyck.greg@hotmail.fr>
2015-12-02 11:45:27 +0100

    Update Pion.java

--- jeudeloie/src/be/ephec/Player/Pion.java
+++ jeudeloie/src/be/ephec/Player/Pion.java
@@ -1,5 +1,20 @@
 package be.ephec.Player;
 
-public class Pion {
+public abstract class Pion {
+	private int numCase;
+	private int posX;
+	private int posY;
+	
+	  public int getPosX() {
+		    return posX;
+		  }
+	  public int getPosY() {
+		    return posY;
+		  }
+	  public void setCoord(int posX, int posY) {
+		    this.posX = posX;
+		    this.posY = posY;
+		  }
 
-}
+	}
+	
