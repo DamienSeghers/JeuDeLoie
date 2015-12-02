88bb842b3c76852e23b9401dca6c5c46bc6feac3
gpk659 <pyck.greg@hotmail.fr>
2015-12-02 22:46:38 +0100

    Update Joueur.java

--- jeudeloie/src/be/ephec/Player/Joueur.java
+++ jeudeloie/src/be/ephec/Player/Joueur.java
@@ -1,6 +1,18 @@
 package be.ephec.Player;
 
 public class Joueur { //initialise un joueur 
-	String nom = "Damien";
-	int position = 0;
+	private String nom;
+	private int position = 0; //position du joueur, l'endroit ou il est
+	private boolean etat;
+	
+	Joueur joueur1 = new Joueur(); //creation de l'objet joueur
+	
+	//constructeur
+	public Joueur(){
+		//création d'un joueur
+		nom = "joueur1"; //nom du joueur
+		etat = false; //etat du joueur
+		position; //position à 0 de base
+	}
+	
 }
