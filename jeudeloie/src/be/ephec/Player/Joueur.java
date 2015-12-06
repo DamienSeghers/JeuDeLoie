8cac19f6ad5faa25f5833ea12aea7192e479e034
gpk659 <pyck.greg@hotmail.fr>
2015-12-06 11:59:59 +0100

    Update Joueur.java

--- jeudeloie/src/be/ephec/Player/Joueur.java
+++ jeudeloie/src/be/ephec/Player/Joueur.java
@@ -1,5 +1,6 @@
 package be.ephec.Player;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Joueur {
@@ -8,15 +9,13 @@ public class Joueur {
 	private int position = 0;
 	private Pion p1;
 	
-	Joueur joueur1 = new Joueur("Joueur1", false, 0);
+	Joueur joueur1 = new Joueur("Joueur1", false, 0,Color.red);
 	
-	public Joueur(String nomJoueur, boolean etatJoueur, int positionJoueur){
+	public Joueur(String nomJoueur, boolean etatJoueur, int positionJoueur, Color couleur){
 		//création d'un joueur
 		nom = nomJoueur; //nom du joueur
 		etat = etatJoueur;//etat du joueur
 		position = positionJoueur;  //position du joeur
-		Pion p = new Pion(null, 20,20);
-
+		Pion p = new Pion(null,couleur,20,20); 
 		}
-	
 }
