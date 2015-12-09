a5b279e52cf8e221fe7be68f90d6805b828df0ce
simonbarre <simon.barre@gmail.com>
2015-12-09 12:15:44 +0100

    Update Fenetre.java

--- jeudeloie/src/be/ephec/UserInterface/Fenetre.java
+++ jeudeloie/src/be/ephec/UserInterface/Fenetre.java
@@ -1,28 +1,46 @@
 package be.ephec.UserInterface;
 
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Fenetre extends JFrame {
 	public Fenetre(){
 		
-		/*
-		 * La fenêtre 
-		 */
+		/*La fenÃªtre */
+		this.setTitle("Jeu de l'oie"); 								// Donne une titre Ã  la fenÃªtre
+		this.setSize(800, 500); 									// DÃ©finis la taille de la fenÃªtre
+		this.setLocationRelativeTo(null); 							// Mets la fenÃªtre au centre
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 		// Ferme la fenÃªtre quand on clique sur la croix
+		this.setContentPane(new Fond()); 							// Appelle Fond pour faire le background
+		this.setVisible(true); 										// Rend la fenÃªtre visisble
 		
-		this.setTitle("Jeu de l'oie"); // Donne une titre à la fenêtre
-		this.setSize(800, 500); // Définis la taille de la fenêtre
-		this.setLocationRelativeTo(null); // Mets la fenêtre au centre
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ferme la fenêtre quand on clique sur la croix
-		this.setContentPane(new Fond()); // Appelle Fond pour faire le background
+		/*L'image de fond */
+		JPanel fond = new JPanel(); 								// Instancie un objet JPanel
 		
-		this.setVisible(true); // Rend la fenêtre visisble
+		/*Declaration des Label*/
+		JLabel labelTitle = new JLabel("Jeu de l'oie"); 			//Pour ecrire le titre du jeu dans la fenetre
+		labelTitle.setBounds(265, 10, 150, 40);						//Taille & position de la fenetre
+		labelTitle.setFont(new Font("Tahoma", Font.PLAIN, 30));		//Changer la police du texte
 		
-		/*
-		 * L'image de fond 
-		 */
+		JLabel labelText1 = new JLabel("C'est au tour de : ");		//Pour ecrire dans la fenetre
+		labelText1.setBounds(800, 60, 100, 15);						//Taille & position de la fenetre
 		
-		JPanel fond = new JPanel(); // Instancie un objet JPanel
+		JLabel labelText2 = new JLabel("JoueurName");				//Pour ecrire dans la fenetre
+		labelText2.setBounds(500, 60, 150, 15);						//Taille & position de la fenetre
+		
+		JButton boutonLancerDe = new JButton("Lancer dÃ©");			//pour creer le bouton
+		boutonLancerDe.setBounds(582, 80, 105, 25);					//Taille & position de la fenetre
+		
+		//ajout des element sur le fond
+		fond.setLayout(null);
+		fond.add(labelText2);
+		fond.add(labelTitle);
+		fond.add(labelText1);
+		fond.add(boutonLancerDe);
 	}
 
 }
