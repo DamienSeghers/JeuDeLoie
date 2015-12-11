68431289ef442e6fddc6a877a28792720654d561
DamienSeghers <damien.seghers@hotmail.com>
2015-12-11 09:14:14 +0100

    Update JeuBackground.java

--- jeudeloie/src/be/ephec/TestandDebug/JeuBackground.java
+++ jeudeloie/src/be/ephec/TestandDebug/JeuBackground.java
@@ -1,40 +1,37 @@
 package be.ephec.TestandDebug;
 
 import java.util.Scanner;
-import java.math.*;
+import java.math.*; // test bureau github
 
 public class JeuBackground {
 
-	
 	public static void main(String[] args) {
 		
 		int cpt =0;
 		int actionPlayer;
 		Joueur player1 = new Joueur();
 		Event event = new Event();
-		Dé de = new Dé();
-
+		DÃ© de = new DÃ©();
+		
 		do {
 			player1.setSonTour(true);
+			cpt = cpt +1;
 			while(player1.isSonTour())
 			{
-				System.out.println( "/////"+player1.getPosition());
+				System.out.println( " DÃ©but du tour "+cpt+" en case "+player1.getPosition());
 				de.lancerDe();
-				System.out.println("testPosition =" + player1.getPosition() +" dé = " + de.getResultat());
-				player1.setPosition(player1.getPosition() + de.getResultat()); //change la position après le lancer de dé
-				System.out.println("Position change :" + player1.getPosition());
-				actionPlayer = event.checkEvent(player1.getPosition()); //vérifie si action sur la case 
-				if(actionPlayer == 1)
-					System.out.println("Position : " + player1.getPosition() + " IL BOUGEEEEEEE");
+				System.out.println("dÃ© = " + de.getResultat());
+				player1.setPosition(player1.getPosition() + de.getResultat()); //change la position aprÃ¨s le lancer de dÃ©
+				System.out.println("Nouvelle case :" + player1.getPosition());
+				actionPlayer = event.checkEvent(player1.getPosition()); //vÃ©rifie si action sur la case
 				player1.faireAction(actionPlayer);
-				cpt = cpt +1;
-				System.out.println("position = " + player1.getPosition());
-				System.out.println("Fin du tour"+cpt);
+				System.out.println("il bouge Ã  la case " + player1.getPosition()); 
+				System.out.println("Fin du tour "+cpt+" en case "+player1.getPosition());
+				System.out.println(" ");
+				System.out.println(" ");
 			}
-			
 		}
 		while (player1.getPosition() < 31);
-		
-		System.out.println("Bravo Tu as fini le jeu Connard ... ");
+		System.out.println("Win !!");
 	}
 }
