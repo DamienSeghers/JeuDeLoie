930ea520a68cb0c0b00f70862dc18ef387f4b08c
simonbarre <simon.barre@gmail.com>
2015-12-11 08:54:47 +0100

    Update Fenetre2.java

--- jeudeloie/src/be/ephec/UserInterface/Fenetre2.java
+++ jeudeloie/src/be/ephec/UserInterface/Fenetre2.java
@@ -1,32 +1,20 @@
 package be.ephec.UserInterface;
 
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+
 
 
 public class Fenetre2 {
 
 	private JFrame frmJeuDeLoie;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Fenetre2 window = new Fenetre2();
-					window.frmJeuDeLoie.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the application.
 	 */
@@ -45,27 +33,59 @@ public class Fenetre2 {
 		frmJeuDeLoie.setLocationRelativeTo(null);
 		Fond fond = new Fond();
 		frmJeuDeLoie.setContentPane(fond);
-		System.out.println(frmJeuDeLoie.getSize());
-		System.out.println(fond.getSize());
+		fond.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		fond.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		panel.setOpaque(false);
 		
 		JLabel labelTitle = new JLabel("Jeu de l'oie");
-		labelTitle.setBounds(265, 10, 150, 40);
+		panel.add(labelTitle, BorderLayout.NORTH);
+		labelTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		labelTitle.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		
-		JLabel labelText1 = new JLabel("C'est au tour de : ");
-		labelText1.setBounds(100, 60, 100, 15);
-		
-		JLabel labelText2 = new JLabel("JoueurName");
-		labelText2.setBounds(500, 60, 150, 15);
+		labelTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		
 		JButton boutonLancerDe = new JButton("Lancer dé");
-		boutonLancerDe.setBounds(582, 80, 105, 25);
+		panel.add(boutonLancerDe, BorderLayout.EAST);
+		//boutonLancerDe.setSize(300, 50);  														//NOT WORK
 		
-		fond.setLayout(null);
-		fond.add(labelText2);
-		fond.add(labelTitle);
-		fond.add(labelText1);
-		fond.add(boutonLancerDe);
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_1.setOpaque(false);
+		
+		JLabel labelText1 = new JLabel("C'est au tour de : ");
+		panel_1.add(labelText1);
+		labelText1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		
+		JLabel labelText2 = new JLabel("JoueurName");
+		panel_1.add(labelText2);
+		
+		JButton btnNewButton = new JButton("Relancer le jeu");
+		//btnNewButton.setSize(new Dimension(500, 0));  											//NOT WORK
+		btnNewButton.setSize(300, 50);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnNewButton, BorderLayout.WEST);
 	}
-
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fenetre2 window = new Fenetre2();
+					window.frmJeuDeLoie.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
