0accd20bcc2e3956de564105a1960dc4428f7700
gpk659 <pyck.greg@hotmail.fr>
2015-12-12 23:32:31 +0100

    Update Joueur.java

--- jeudeloie/src/be/ephec/Player/Joueur.java
+++ jeudeloie/src/be/ephec/Player/Joueur.java
@@ -3,19 +3,22 @@ package be.ephec.Player;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Joueur {
-	private String nom;
-	private boolean etat = false;
-	private int position = 0;
-	private Pion p;
+public class Joueur extends Pion {
+	public String nom;
+	public boolean etat = false;
+	public int position = 0;
+	public Pion p;
 	
-	Joueur joueur1 = new Joueur("Joueur1", false, 0,Color.red);
+	//Joueur joueur1 = new Joueur("Joueur1", false, 0,Color.red);
 	
-	public Joueur(String nomJoueur, boolean etatJoueur, int positionJoueur, Color couleur){
+	public Joueur(String nomJoueur, boolean etatJoueur, int positionJoueur,Graphics g, Color couleur, int posX, int posY){
+        //Inutile de créer un pion : tu es dans la classe "Joueur"
+        //donc on crée un joueur sachant que Joueur appartient à la superclasse Pion
+        //via la méthode super()
+		super(g,couleur, posX, posY);
 		//création d'un joueur
 		nom = nomJoueur; //nom du joueur
 		etat = etatJoueur;//etat du joueur
-		position = positionJoueur;  //position du joeur
-		Pion p = new Pion(null,couleur,20,20);		//explique a quoi sert null
-		}
+		position = positionJoueur;  //position du joueur	
+	}
 }
