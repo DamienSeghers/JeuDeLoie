762e711385c52d2b43c9c95030b12659df06f51a
gpk659 <pyck.greg@hotmail.fr>
2015-12-12 23:33:45 +0100

    Update Pion.java

--- jeudeloie/src/be/ephec/Player/Pion.java
+++ jeudeloie/src/be/ephec/Player/Pion.java
@@ -2,33 +2,38 @@ package be.ephec.Player;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JPanel;
 
-public class Pion extends JPanel{
+public class Pion{
 	private int numCase;
 	private int posX;
 	public int posY;
 	public Color couleur;
 	
-	public Pion(Graphics p,Color couleur, int posX, int posY){
-		p.setColor(couleur);
-		p.fillOval(posX, posY, 45, 45);
+	public Pion(){
+		
 	}
 	
-	public Color getColor(){
+	public Pion(Graphics p,Color couleur, int posX, int posY){
+		p.setColor(couleur); //change la couleur
+		p.fillOval(posX, posY, 40, 40);// fait une forme ronde
+	}
+	
+
+	public Color getColor(){ //affiche la couleur
 		return couleur;
 	}
-	public void setColor(Color couleur){
+	public void setColor(Color couleur){ //recupere la couleur
 		this.couleur = couleur;
 	}
 	
+	//afficher les coordnonées 
 	public int getPosX() {
 		    return posX;
 		  }
 	  public int getPosY() {
 		    return posY;
 		  }
-	  public void setCoord(int posX, int posY) {
+	  public void setCoord(int posX, int posY) { //recupere les coordonnées
 		    this.posX = posX;
 		    this.posY = posY;
 		  }
