fb6b27a135de125c4aed1b377a5b6631c9cd67fc
simonbarre <simon.barre@gmail.com>
2015-12-17 18:19:52 +0100

    Update Serveur.java

--- jeudeloie/src/be/ephec/socket/Serveur.java
+++ jeudeloie/src/be/ephec/socket/Serveur.java
@@ -1,20 +1,33 @@
-package be.ephec.Socket;
+package be.ephec.Network;
 
 
 import java.net.*;
 import java.io.*;
 
 public class Serveur {
-
-public static void main(String[] args) throws IOException, ClassNotFoundException {
 	
-	ServerSocket server = new ServerSocket(0);
-	System.out.println("L'adresse du serveur est : " + InetAddress.getLocalHost().getHostAddress() + " et le port : " + server.getLocalPort());
-	Socket socket = server.accept();
-	ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-	System.out.println(input.readObject());
-	socket.close();
-	System.exit(0);
+	private String srvIP;
+	private int srvPort;
+
+	public void server (/*String[] args*/) throws IOException, ClassNotFoundException {
+	
+		ServerSocket server = new ServerSocket(6666);  // 0 pour port automatique
+		srvIP = InetAddress.getLocalHost().getHostAddress();
+		srvPort = server.getLocalPort();
+		System.out.println("L'adresse du serveur est : " + InetAddress.getLocalHost().getHostAddress() + " et le port : " + server.getLocalPort());
+		Socket socket = server.accept();
+		ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+		System.out.println(input.readObject());
+		socket.close();
+		System.exit(0);
+	}
+
+	public String getSrvIP() {
+		return srvIP;
 	}
 	
+	public int getSrvPort() {
+		return srvPort;
+	}
 }
