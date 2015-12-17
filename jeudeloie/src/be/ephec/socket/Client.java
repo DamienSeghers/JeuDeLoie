13c06b4663aaa835b155764652bc41e5683ce3a1
simonbarre <simon.barre@gmail.com>
2015-12-17 18:19:30 +0100

    Update Client.java

--- jeudeloie/src/be/ephec/socket/Client.java
+++ jeudeloie/src/be/ephec/socket/Client.java
@@ -1,14 +1,16 @@
-package be.ephec.Socket;
+package be.ephec.Network;
+
 import javax.swing.JOptionPane;
 import java.net.*;
 import java.io.*;
 
 public class Client {
-	public static void main(String[] args) throws IOException {
+	public static void client (String srvIP) throws IOException {
 		String serverAddress = JOptionPane.showInputDialog("Adresse du Serveur SVP: ");
 		String serverPort = JOptionPane.showInputDialog("Port du Serveur SVP: ");
-		Socket socket = new Socket(serverAddress, Integer.parseInt(serverPort));
+		Socket socket = new Socket(srvIP/*serverAddress*/, 6666 /*Integer.parseInt(serverPort)*/);
 		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+		ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 		output.writeObject("Bonjour, je suis un client, et mon addresse IP est : "  + InetAddress.getLocalHost().getHostAddress());
 		socket.close();
 		System.exit(0);
